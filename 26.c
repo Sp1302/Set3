e5e64e7b5c7c502d790268df5e7781c1c49dc387
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-21 13:53:59 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,31 @@
+# Set3
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,j,temp,n;
+printf("\nenter the number");
+scanf("%d",&n);
+printf("\nenter the number");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+getch();
+}
