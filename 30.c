e712158bf0071cc0cb5067c72513472ef8661659
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-21 14:11:49 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,14 @@
+# Set3
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int hr,min,hr1,min1,hr2,min2;
+printf("\nenter 2 times in hour and minute format");
+scanf("%d%d",&hr1,&min1);
+scanf("%d%d",&hr2,&min2);
+hr=hr2-hr1;
+min=min2-min1;
+printf("%d\t%d\n",hr,min);
+return 0;
+}
